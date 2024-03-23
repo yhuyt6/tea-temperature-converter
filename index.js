758c1367035df9a3e5e8fb1b33b966448edc22f5
dfghjkcvbnmnbvcx <163177682+yhuyt6@users.noreply.github.com>
2024-03-23 19:40:26 +0700

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+
+function fahrenheitToCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5/9;
+}
+
+module.exports = { celsiusToFahrenheit, fahrenheitToCelsius };
